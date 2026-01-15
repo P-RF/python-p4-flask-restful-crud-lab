8cc9cfc088d6b28385a2affe3d7709db2d960321
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2026-01-15 16:01:40 -0500

    GET and POST /plant routes

--- server/app.py
+++ server/app.py
@@ -44,8 +44,11 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            return make_response({"error": "Plant not found."}, 404)
+        return make_response(jsonify(plant.to_dict()), 200)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
